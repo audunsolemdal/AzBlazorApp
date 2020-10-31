4ef06efdd4459a131614893af7c78484e91e78eb
audunsolemdal <auls@equinor.com>
2020-10-31 21:24:03 +0100

    Update IBlobStorageService

--- AzBlazorApp/Data/Services/BlobStorageService.cs
+++ AzBlazorApp/Data/Services/BlobStorageService.cs
@@ -1,9 +1,41 @@
 ﻿using AzBlazorApp.Data.Services.Interfaces;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AzBlazorApp.Data.Services
 {
     public class BlobStorageService : IBlobStorageService
     {
+        public Task CreateContainer(string name, BlobServiceClient client)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public Task DeleteContainer(string name, BlobServiceClient client)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<BlobItem> GetAllBlobs()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<BlobContainerItem> GetAllContainers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BlobServiceClient GetBlobStorageClient(string connString)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UploadContentToContainer(string localFilePath)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- AzBlazorApp/Data/Services/Interfaces/IBlobStorageService.cs
+++ AzBlazorApp/Data/Services/Interfaces/IBlobStorageService.cs
@@ -1,6 +1,17 @@
-﻿namespace AzBlazorApp.Data.Services.Interfaces
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AzBlazorApp.Data.Services.Interfaces
 {
     interface IBlobStorageService
     {
+        BlobServiceClient GetBlobStorageClient(string connString);
+        IList<BlobItem> GetAllBlobs();
+        IList<BlobContainerItem> GetAllContainers();
+        Task UploadContentToContainer(string localFilePath);
+        Task CreateContainer(string name, BlobServiceClient client);
+        Task DeleteContainer(string name, BlobServiceClient client);
     }
 }
